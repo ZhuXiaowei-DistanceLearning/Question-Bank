947c0975ed486dcdefb0276dae52abfa61fb3dd2
zxw <502513206@qq.com>
2020-06-24 14:17:02 +0800

    dp题目

--- src/main/java/com/zxw/leetcode/algorithm/Daily.java
+++ src/main/java/com/zxw/leetcode/algorithm/Daily.java
@@ -12,6 +12,9 @@ public class Daily {
     public static void main(String[] args) {
         Daily daily = new Daily();
         daily.kidsWithCandies(new int[]{2, 3, 5, 1, 3}, 3);
+        daily.shuffle(new int[]{2,5,1,3,4,7},3);
+        daily.subtractProductAndSum(234);
+        daily.defangIPaddr("1.1.1.1");
     }
 
     /**
@@ -84,7 +87,7 @@ public class Daily {
         List<Boolean> ans = new ArrayList<>();
         int max = candies[0];
         for (int i = 1; i < candies.length; i++) {
-            max = Math.max(max,candies[i]);
+            max = Math.max(max, candies[i]);
         }
         for (int i = 0; i < candies.length; i++) {
             if (candies[i] + extraCandies >= max) {
@@ -95,4 +98,232 @@ public class Daily {
         }
         return ans;
     }
+
+    /**
+     * 1342. 将数字变成 0 的操作次数
+     * 给你一个非负整数 num ，请你返回将它变成 0 所需要的步数。 如果当前数字是偶数，你需要把它除以 2 ；否则，减去 1 。
+     * 示例 1：
+     * 输入：num = 14
+     * 输出：6
+     * 解释：
+     * 步骤 1) 14 是偶数，除以 2 得到 7 。
+     * 步骤 2） 7 是奇数，减 1 得到 6 。
+     * 步骤 3） 6 是偶数，除以 2 得到 3 。
+     * 步骤 4） 3 是奇数，减 1 得到 2 。
+     * 步骤 5） 2 是偶数，除以 2 得到 1 。
+     * 步骤 6） 1 是奇数，减 1 得到 0 。
+     *
+     * @param num
+     * @return
+     */
+    public int numberOfSteps(int num) {
+        int count = 0;
+        while (num != 0) {
+            num = (num & 1) == 1 ? num - 1 : num / 2;
+            count++;
+        }
+        return count;
+    }
+
+    /**
+     * 771. 宝石与石头
+     *  给定字符串J 代表石头中宝石的类型，和字符串 S代表你拥有的石头。 S 中每个字符代表了一种你拥有的石头的类型，你想知道你拥有的石头中有多少是宝石。
+     * J 中的字母不重复，J 和 S中的所有字符都是字母。字母区分大小写，因此"a"和"A"是不同类型的石头。
+     * 示例 1:
+     * 输入: J = "aA", S = "aAAbbbb"
+     * 输出: 3
+     *
+     * @param J
+     * @param S
+     * @return
+     */
+    public int numJewelsInStones(String J, String S) {
+        int ans = 0;
+        for (int i = 0; i < J.length(); i++) {
+            for (int j = 0; j < S.length(); j++) {
+                if (J.charAt(i) == S.charAt(j)) {
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+
+    /**
+     * LCP 01. 猜数字
+     * 小A 和 小B 在玩猜数字。小B 每次从 1, 2, 3 中随机选择一个，小A 每次也从 1, 2, 3 中选择一个猜。他们一共进行三次这个游戏，请返回 小A 猜对了几次？
+     * 输入的guess数组为 小A 每次的猜测，answer数组为 小B 每次的选择。guess和answer的长度都等于3。
+     * 示例 1：
+     * 输入：guess = [1,2,3], answer = [1,2,3]
+     * 输出：3
+     * 解释：小A 每次都猜对了。
+     *
+     * @param guess
+     * @param answer
+     * @return
+     */
+    public int game(int[] guess, int[] answer) {
+        int ans = 0;
+        for (int i = 0; i < guess.length; i++) {
+            if (guess[i] == answer[i]) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+
+    /**
+     * 1470. 重新排列数组
+     * 给你一个数组 nums ，数组中有 2n 个元素，按 [x1,x2,...,xn,y1,y2,...,yn] 的格式排列。
+     * 请你将数组按 [x1,y1,x2,y2,...,xn,yn] 格式重新排列，返回重排后的数组。
+     * 输入：nums = [2,5,1,3,4,7], n = 3
+     * 示例 1：
+     * 输出：[2,3,5,4,1,7]
+     * 解释：由于 x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 ，所以答案为 [2,3,5,4,1,7]
+     *
+     * @param nums
+     * @param n
+     * @return
+     */
+    public int[] shuffle(int[] nums, int n) {
+        int[] arr = new int[nums.length];
+        int length = nums.length / 2;
+        for (int i = 0; i < length;i++) {
+            if (i == n - 1) {
+                arr[nums.length - 1] = nums[i + length];
+                arr[nums.length - 2] = nums[i];
+                continue;
+            }
+            arr[i * 2] = nums[i];
+            arr[i * 2 + 1] = nums[i + length];
+        }
+        return arr;
+    }
+
+    /**
+     * 1281. 整数的各位积和之差
+     * 给你一个整数 n，请你帮忙计算并返回该整数「各位数字之积」与「各位数字之和」的差。
+     * 示例 1：
+     * <p>
+     * 输入：n = 234
+     * 输出：15
+     * 解释：
+     * 各位数之积 = 2 * 3 * 4 = 24
+     * 各位数之和 = 2 + 3 + 4 = 9
+     * 结果 = 24 - 9 = 15
+     *
+     * @param n
+     * @return
+     */
+    public int subtractProductAndSum(int n) {
+        int mult = 1;
+        int sum = 0;
+        while (n > 0) {
+            int mid = n % 10;
+            n = n / 10;
+            mult = mult * mid;
+            sum = sum + mid;
+        }
+        return mult - sum;
+    }
+
+    /**
+     * 1108. IP 地址无效化
+     * 给你一个有效的 IPv4 地址 address，返回这个 IP 地址的无效化版本。
+     * 所谓无效化 IP 地址，其实就是用 "[.]" 代替了每个 "."。
+     * 示例 1：
+     * 输入：address = "1.1.1.1"
+     * 输出："1[.]1[.]1[.]1"
+     * @param address
+     * @return
+     */
+    public String defangIPaddr(String address) {
+        return address.replaceAll("\\.","[.]");
+    }
+
+    /**
+     * 1295. 统计位数为偶数的数字给你一个整数数组 nums，请你返回其中位数为 偶数 的数字的个数。
+     * 示例 1：
+     * 输入：nums = [12,345,2,6,7896]
+     * 输出：2
+     * 解释：
+     * 12 是 2 位数字（位数为偶数） 
+     * 345 是 3 位数字（位数为奇数）  
+     * 2 是 1 位数字（位数为奇数） 
+     * 6 是 1 位数字 位数为奇数） 
+     * 7896 是 4 位数字（位数为偶数）  
+     * 因此只有 12 和 7896 是位数为偶数的数字
+     * @param nums
+     * @return
+     */
+    public int findNumbers(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (String.valueOf(nums[i]).length() % 2 == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    /**
+     * 1365. 有多少小于当前数字的数字
+     * 给你一个数组 nums，对于其中每个元素 nums[i]，请你统计数组中比它小的所有数字的数目。
+     * 换而言之，对于每个 nums[i] 你必须计算出有效的 j 的数量，其中 j 满足 j != i 且 nums[j] < nums[i] 。
+     * 以数组形式返回答案。
+     * 示例 1：
+     * 输入：nums = [8,1,2,2,3]
+     * 输出：[4,0,1,1,3]
+     * 解释：
+     * 对于 nums[0]=8 存在四个比它小的数字：（1，2，2 和 3）。
+     * 对于 nums[1]=1 不存在比它小的数字。
+     * 对于 nums[2]=2 存在一个比它小的数字：（1）。
+     * 对于 nums[3]=2 存在一个比它小的数字：（1）。
+     * 对于 nums[4]=3 存在三个比它小的数字：（1，2 和 2）。
+     * @param nums
+     * @return
+     */
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] arr = new int[nums.length];
+        for (int i = 0; i < arr.length; i++) {
+            int c = 0;
+            for (int j = 0; j < arr.length; j++) {
+                if(i == j){
+                    continue;
+                }
+                if(nums[i] > nums[j]){
+                    c++;
+                }
+            }
+            arr[i] = c;
+        }
+        return arr;
+    }
+
+
+    /**
+     * 1313. 解压缩编码列表
+     * 给你一个以行程长度编码压缩的整数列表 nums 。
+     * 考虑每对相邻的两个元素 [freq, val] = [nums[2*i], nums[2*i+1]] （其中 i >= 0 ），每一对都表示解压后子列表中有 freq 个值为 val 的元素，你需要从左到右连接所有子列表以生成解压后的列表。
+     * 请你返回解压后的列表。
+     * 示例：
+     * 输入：nums = [1,2,3,4]
+     * 输出：[2,4,4,4]
+     * 解释：第一对 [1,2] 代表着 2 的出现频次为 1，所以生成数组 [2]。
+     * 第二对 [3,4] 代表着 4 的出现频次为 3，所以生成数组 [4,4,4]。
+     * 最后将它们串联到一起 [2] + [4,4,4] = [2,4,4,4]。
+     * @param nums
+     * @return
+     */
+    public int[] decompressRLElist(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums.length / 2; i++) {
+            while (nums[i * 2] != 0){
+                list.add(nums[i * 2 + 1]);
+                nums[i * 2]--;
+            }
+        }
+        return list.stream().mapToInt(Integer::valueOf).toArray();
+    }
+
 }
--- src/main/java/com/zxw/leetcode/algorithm/JuneDay.java
+++ src/main/java/com/zxw/leetcode/algorithm/JuneDay.java
@@ -24,16 +24,16 @@ public class JuneDay {
     public boolean isPalindrome(String s) {
         s = s.toLowerCase();
         int l = 0, r = s.length() - 1;
-        while (l < r){
-            if(!Character.isLetterOrDigit(s.charAt(l))){
+        while (l < r) {
+            if (!Character.isLetterOrDigit(s.charAt(l))) {
                 l++;
                 continue;
             }
-            if(!Character.isLetterOrDigit(s.charAt(r))){
+            if (!Character.isLetterOrDigit(s.charAt(r))) {
                 r++;
                 continue;
             }
-            if(s.charAt(l) != s.charAt(r)){
+            if (s.charAt(l) != s.charAt(r)) {
                 return false;
             }
             l++;
@@ -45,13 +45,14 @@ public class JuneDay {
     /**
      * 面试题 16.18. 模式匹配
      * 你有两个字符串，即pattern和value。 pattern字符串由字母"a"和"b"组成，用于描述字符串中的模式。例如，字符串"catcatgocatgo"匹配模式"aabab"（其中"cat"是"a"，"go"是"b"），该字符串也匹配像"a"、"ab"和"b"这样的模式。但需注意"a"和"b"不能同时表示相同的字符串。编写一个方法判断value字符串是否匹配pattern字符串。
-     *
+     * <p>
      * 示例 1：
-     *
+     * <p>
      * 输入： pattern = "abba", value = "dogcatcatdog"
      * 输出： true
-     *
+     * <p>
      * 你可以假设pattern只包含字母"a"和"b"，value仅包含小写字母。
+     *
      * @param pattern
      * @param value
      * @return
@@ -66,15 +67,15 @@ public class JuneDay {
      * 输入为 非空 字符串且只包含数字 1 和 0。
      * 示例 1:
      * 输入: a = "11", b = "1"
-     * @param a
-     * 输出: "100"
+     *
+     * @param a 输出: "100"
      * @param b
      * @return
      */
     public String addBinary(String a, String b) {
         StringBuilder ans = new StringBuilder();
         int ca = 0;
-        for(int i = a.length() - 1, j = b.length() - 1;i >= 0 || j >= 0; i--, j--) {
+        for (int i = a.length() - 1, j = b.length() - 1; i >= 0 || j >= 0; i--, j--) {
             int sum = ca;
             sum += i >= 0 ? a.charAt(i) - '0' : 0;
             sum += j >= 0 ? b.charAt(j) - '0' : 0;
@@ -84,4 +85,21 @@ public class JuneDay {
         ans.append(ca == 1 ? ca : "");
         return ans.reverse().toString();
     }
+
+    /**
+     * 16. 最接近的三数之和
+     * 给定一个包括 n 个整数的数组 nums 和 一个目标值 target。找出 nums 中的三个整数，使得它们的和与 target 最接近。返回这三个数的和。假定每组输入只存在唯一答案。
+     * 示例：
+     * 输入：nums = [-1,2,1,-4], target = 1
+     * 输出：2
+     * 解释：与 target 最接近的和是 2 (-1 + 2 + 1 = 2) 。
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int threeSumClosest(int[] nums, int target) {
+        return 0;
+    }
+
 }
