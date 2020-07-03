a59c7aebd643df4c547466b1e8b28ecefb68cf98
zxw <502513206@qq.com>
2020-07-03 15:49:25 +0800

    dp题目

--- src/main/java/com/zxw/leetcode/algorithm/JuneDay.java
+++ src/main/java/com/zxw/leetcode/algorithm/JuneDay.java
@@ -1,5 +1,7 @@
 package com.zxw.leetcode.algorithm;
 
+import com.zxw.common.datastruct.TreeNode;
+
 import java.util.Arrays;
 
 /**
@@ -225,5 +227,38 @@ public class JuneDay {
             }
         }
         return res;
+    }
+
+    /**
+     * 108. 将有序数组转换为二叉搜索树
+     * 将一个按照升序排列的有序数组，转换为一棵高度平衡二叉搜索树。
+     * 本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
+     * 示例:
+     * 给定有序数组: [-10,-3,0,5,9],
+     * 一个可能的答案是：[0,-3,9,-10,null,5]，它可以表示下面这个高度平衡二叉搜索树：
+     * <p>
+     * 0
+     * / \
+     * -3   9
+     * /   /
+     * -10  5
+     *
+     * @param nums
+     * @return
+     */
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return ArrayToBSTDFS(nums, 0, nums.length - 1);
+    }
+
+    public TreeNode ArrayToBSTDFS(int[] nums, int l, int r) {
+        if (l > r) {
+            return null;
+        }
+        int mid = l + (r - l) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        // 递归的构建 root 的左子树与右子树。
+        root.left = ArrayToBSTDFS(nums, l, mid - 1);
+        root.right = ArrayToBSTDFS(nums, mid + 1, r);
+        return root;
     }
 }
