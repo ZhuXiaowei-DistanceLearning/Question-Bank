340877fbced4b0be509602c6a5d3c99eaacd5aac
zxw <502513206@qq.com>
2020-07-06 15:41:51 +0800

    dp题目

--- src/main/java/com/zxw/leetcode/algorithm/JuneDay.java
+++ src/main/java/com/zxw/leetcode/algorithm/JuneDay.java
@@ -261,4 +261,45 @@ public class JuneDay {
         root.right = ArrayToBSTDFS(nums, mid + 1, r);
         return root;
     }
+
+    /**
+     * 63. 不同路径 II
+     * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
+     * 现在考虑网格中有障碍物。那么从左上角到右下角将会有多少条不同的路径？
+     * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
+     *
+     * @param obstacleGrid
+     * @return
+     */
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int[][] dp = new int[obstacleGrid.length][obstacleGrid[0].length];
+        boolean right = false;
+        boolean down = false;
+        for (int i = 0; i < obstacleGrid.length; i++) {
+            if (obstacleGrid[i][0] == 1 || right) {
+                right = true;
+                dp[i][0] = 0;
+            } else {
+                dp[i][0] = 1;
+            }
+        }
+        for (int i = 0; i < obstacleGrid[0].length; i++) {
+            if (obstacleGrid[0][i] == 1 || down) {
+                down = true;
+                dp[0][i] = 0;
+            } else {
+                dp[0][i] = 1;
+            }
+        }
+        for (int i = 1; i < obstacleGrid.length; i++) {
+            for (int j = 1; j < obstacleGrid[i].length; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+        return dp[dp.length - 1][dp[0].length - 1];
+    }
 }
