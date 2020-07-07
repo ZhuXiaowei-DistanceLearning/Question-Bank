4957b0c87cf6f5e5b88a63f864579d98a9744e6e
zxw <502513206@qq.com>
2020-07-07 11:47:04 +0800

    每日一题

--- src/main/java/com/zxw/leetcode/algorithm/JuneDay.java
+++ src/main/java/com/zxw/leetcode/algorithm/JuneDay.java
@@ -302,4 +302,38 @@ public class JuneDay {
         }
         return dp[dp.length - 1][dp[0].length - 1];
     }
+
+    /**
+     * 112. 路径总和
+     * 给定一个二叉树和一个目标和，判断该树中是否存在根节点到叶子节点的路径，这条路径上所有节点值相加等于目标和。
+     * <p>
+     * 说明: 叶子节点是指没有子节点的节点。
+     * <p>
+     * 示例: 
+     * 给定如下二叉树，以及目标和 sum = 22，
+     * <p>
+     * 5
+     * / \
+     * 4   8
+     * /   / \
+     * 11  13  4
+     * /  \      \
+     * 7    2      1
+     *
+     * @param root
+     * @param sum
+     * @return
+     */
+    int num = 0;
+
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null) {
+            return root.val == sum;
+        }
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+
 }
