e58404cbfe1ffa665c9843bb95c0e3adc079ca33
zxw <502513206@qq.com>
2020-07-08 20:10:04 +0800

    Merge https://github.com/502513206/Question-Bank
    # Conflicts:
    #	src/main/java/com/zxw/leetcode/algorithm/JuneDay.java

--- src/main/java/com/zxw/leetcode/interviewgold/StringText.java
+++ src/main/java/com/zxw/leetcode/interviewgold/StringText.java
@@ -32,4 +32,34 @@ public class StringText {
         }
         return true;
     }
+
+    /**
+     * 面试题 01.02. 判定是否互为字符重排
+     * 给定两个字符串 s1 和 s2，请编写一个程序，确定其中一个字符串的字符重新排列后，能否变成另一个字符串。
+     *
+     * 示例 1：
+     *
+     * 输入: s1 = "abc", s2 = "bca"
+     * 输出: true
+     * @param s1
+     * @param s2
+     * @return
+     */
+    public boolean CheckPermutation(String s1, String s2) {
+        if(s1.length() != s2.length()){
+            return false;
+        }
+        int[] arr = new int[128];
+        for (int i = 0; i < s1.length(); i++) {
+            arr[s1.charAt(i)]++;
+        }
+
+        for (int i = 0; i < s2.length(); i++) {
+            if(arr[s2.charAt(i)] == 0){
+                return false;
+            }
+            arr[s2.charAt(i)]--;
+        }
+        return true;
+    }
 }
