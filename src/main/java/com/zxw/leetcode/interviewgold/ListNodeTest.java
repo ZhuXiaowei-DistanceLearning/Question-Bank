f3d8888c98a22f80b24a9d8a193fb080968e4461
zxw <502513206@qq.com>
2020-08-04 15:47:55 +0800

    面试经典

--- src/main/java/com/zxw/leetcode/interviewgold/ListNodeTest.java
+++ src/main/java/com/zxw/leetcode/interviewgold/ListNodeTest.java
@@ -11,6 +11,11 @@ import java.util.HashSet;
 public class ListNodeTest {
     public static void main(String[] args) {
         ListNodeTest listNodeTest = new ListNodeTest();
+        ListNode node = new ListNode(4);
+        node.next = new ListNode(5);
+        node.next.next = new ListNode(1);
+        node.next.next.next = new ListNode(9);
+        listNodeTest.deleteNode(node);
     }
 
     /**
@@ -64,5 +69,23 @@ public class ListNodeTest {
             head = head.next;
         }
         return node.val;
+    }
+
+    /**
+     * 面试题 02.03. 删除中间节点
+     * 实现一种算法，删除单向链表中间的某个节点（即不是第一个或最后一个节点），假定你只能访问该节点。
+     * 示例：
+     * 输入：单向链表a->b->c->d->e->f中的节点c
+     * 结果：不返回任何数据，但该链表变为a->b->d->e->f
+     *
+     * @param node
+     */
+    public void deleteNode(ListNode node) {
+        ListNode fast = node;
+        ListNode slow = node;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
     }
 }
