94e641e643a50b526c1609746a3e73c30d850746
zxw <502513206@qq.com>
2020-08-05 18:25:05 +0800

    面试经典

--- src/main/java/com/zxw/leetcode/interviewgold/ListNodeTest.java
+++ src/main/java/com/zxw/leetcode/interviewgold/ListNodeTest.java
@@ -65,7 +65,6 @@ public class ListNodeTest {
             } else {
                 node = node.next;
             }
-            ListNode next = head.next;
             head = head.next;
         }
         return node.val;
@@ -87,5 +86,19 @@ public class ListNodeTest {
             fast = fast.next.next;
             slow = slow.next;
         }
+    }
+
+    /**
+     * 面试题 02.04. 分割链表
+     * 编写程序以 x 为基准分割链表，使得所有小于 x 的节点排在大于或等于 x 的节点之前。如果链表中包含 x，x 只需出现在小于 x 的元素之后(如下所示)。分割元素 x 只需处于“右半部分”即可，其不需要被置于左右两部分之间。
+     * 示例:
+     * 输入: head = 3->5->8->5->10->2->1, x = 5
+     * 输出: 3->1->2->10->5->5->8
+     * @param head
+     * @param x
+     * @return
+     */
+    public ListNode partition(ListNode head, int x) {
+            return null;
     }
 }
