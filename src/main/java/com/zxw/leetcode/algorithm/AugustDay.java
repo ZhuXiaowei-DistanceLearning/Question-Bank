c5f568f8af138c8451728522316ad871319b222f
zxw <502513206@qq.com>
2020-08-14 09:46:18 +0800

    面试经典

--- src/main/java/com/zxw/leetcode/algorithm/AugustDay.java
+++ src/main/java/com/zxw/leetcode/algorithm/AugustDay.java
@@ -2,6 +2,10 @@ package com.zxw.leetcode.algorithm;
 
 import com.zxw.common.datastruct.TreeNode;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
 /**
  * @author zxw
  * @date 2020/8/7 9:21
@@ -9,6 +13,7 @@ import com.zxw.common.datastruct.TreeNode;
 public class AugustDay {
     public static void main(String[] args) {
         AugustDay augustDay = new AugustDay();
+        augustDay.isValid("");
     }
 
     /**
@@ -17,29 +22,68 @@ public class AugustDay {
      * 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
      * 示例 1:
      * 输入:       1         1
-     *           / \       / \
-     *          2   3     2   3
-     *
-     *         [1,2,3],   [1,2,3]
+     * / \       / \
+     * 2   3     2   3
+     * <p>
+     * [1,2,3],   [1,2,3]
      * 输出: true
+     *
      * @param p
      * @param q
      * @return
      */
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p == null && q != null){
+        if (p == null && q != null) {
             return false;
         }
-        if(p != null && q == null){
+        if (p != null && q == null) {
             return false;
         }
-        if(p.val != q.val){
+        if (p.val != q.val) {
             return false;
         }
-        if(p == null && q == null){
+        if (p == null && q == null) {
             return true;
         }
-        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+    /**
+     * 20. 有效的括号
+     * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+     * 有效字符串需满足：
+     * 左括号必须用相同类型的右括号闭合。
+     * 左括号必须以正确的顺序闭合。
+     * 注意空字符串可被认为是有效字符串。
+     * 示例 1:
+     * 输入: "()"
+     * 输出: true
+     *
+     * @param s
+     * @return
+     */
+    public boolean isValid(String s) {
+        if(s.length() == 1){
+            return false;
+        }
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> map = new HashMap();
+
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (map.containsKey(c)) {
+                char c1 = stack.empty() ? ' ' : stack.pop();
+                if (c1 != map.get(c)) {
+                    return false;
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+        return true;
     }
 
 }
