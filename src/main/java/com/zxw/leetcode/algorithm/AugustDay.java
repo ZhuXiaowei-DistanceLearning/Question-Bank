a2bd94efa8d40596a52e5bf9295e346cc7976742
zxw <502513206@qq.com>
2020-08-16 10:11:22 +0800

    面试经典

--- src/main/java/com/zxw/leetcode/algorithm/AugustDay.java
+++ src/main/java/com/zxw/leetcode/algorithm/AugustDay.java
@@ -63,7 +63,7 @@ public class AugustDay {
      * @return
      */
     public boolean isValid(String s) {
-        if(s.length() == 1){
+        if (s.length() == 1) {
             return false;
         }
         Stack<Character> stack = new Stack<>();
@@ -86,4 +86,56 @@ public class AugustDay {
         return true;
     }
 
+    /**
+     * 733. 图像渲染
+     * 有一幅以二维整数数组表示的图画，每一个整数表示该图画的像素值大小，数值在 0 到 65535 之间。
+     * 给你一个坐标 (sr, sc) 表示图像渲染开始的像素值（行 ，列）和一个新的颜色值 newColor，让你重新上色这幅图像。
+     * 为了完成上色工作，从初始坐标开始，记录初始坐标的上下左右四个方向上像素值与初始坐标相同的相连像素点，接着再记录这四个方向上符合条件的像素点与他们对应四个方向上像素值与初始坐标相同的相连像素点，……，重复该过程。将所有有记录的像素点的颜色值改为新的颜色值。
+     * 最后返回经过上色渲染后的图像。
+     * 示例 1:
+     * 输入:
+     * image = [[1,1,1],[1,1,0],[1,0,1]]
+     * sr = 1, sc = 1, newColor = 2
+     * 输出: [[2,2,2],[2,2,0],[2,0,1]]
+     * 解析:
+     * 在图像的正中间，(坐标(sr,sc)=(1,1)),
+     * 在路径上所有符合条件的像素点的颜色都被更改成2。
+     * 注意，右下角的像素没有更改为2，
+     * 因为它不是在上下左右四个方向上与初始点相连的像素点。
+     *
+     * @param image
+     * @param sr
+     * @param sc
+     * @param newColor
+     * @return
+     */
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        int origColor = image[sr][sc];
+        fill(image, sr, sc, origColor, newColor);
+        return image;
+    }
+
+    void fill(int[][] image, int x, int y,
+              int origColor, int newColor) {
+        // 出界：超出数组边界
+        if (!inArea(image, x, y)) return;
+        // 碰壁：遇到其他颜色，超出 origColor 区域
+        if (image[x][y] != origColor) return;
+        // 已探索过的 origColor 区域
+        if (image[x][y] == -1) return;
+
+        // choose：打标记，以免重复
+        image[x][y] = -1;
+        fill(image, x, y + 1, origColor, newColor);
+        fill(image, x, y - 1, origColor, newColor);
+        fill(image, x - 1, y, origColor, newColor);
+        fill(image, x + 1, y, origColor, newColor);
+        // unchoose：将标记替换为 newColor
+        image[x][y] = newColor;
+    }
+
+    boolean inArea(int[][] image, int x, int y) {
+        return x >= 0 && x < image.length
+                && y >= 0 && y < image[0].length;
+    }
 }
--- src/main/java/com/zxw/leetcode/interviewgold/ListNodeTest.java
+++ src/main/java/com/zxw/leetcode/interviewgold/ListNodeTest.java
@@ -106,6 +106,56 @@ public class ListNodeTest {
     }
 
     /**
+     * 面试题 02.05. 链表求和
+     * 给定两个用链表表示的整数，每个节点包含一个数位。
+     * 这些数位是反向存放的，也就是个位排在链表首部。
+     * 编写函数对这两个整数求和，并用链表形式返回结果。
+     * 示例：
+     * 输入：(7 -> 1 -> 6) + (5 -> 9 -> 2)，即617 + 295
+     * 输出：2 -> 1 -> 9，即912
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        boolean flag = false;
+        ListNode listNode = l1;
+        while (l1 != null && l2 != null) {
+            int i = l1.val + l2.val;
+            if (flag) {
+                i++;
+            }
+            if (i < 10) {
+                l1.val = i;
+                flag = false;
+            } else {
+                l1.val = i % 10;
+                flag = true;
+            }
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+        if (l2 != null) {
+            if (l1 == null) {
+                listNode.next = l2;
+            } else {
+                l1.next = l2;
+            }
+        }
+        if (flag) {
+            listNode.next = new ListNode(1);
+        }
+        return listNode;
+    }
+
+    /**
      * 面试题 02.06. 回文链表
      * 编写一个函数，检查输入的链表是否是回文的。
      * 示例 1：
@@ -181,7 +231,7 @@ public class ListNodeTest {
         while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
-            if(slow == fast){
+            if (slow == fast) {
                 break;
             }
         }
