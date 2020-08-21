cc309f195991eb982dee9a4e7c53cc57fea5d6f1
zxw <502513206@qq.com>
2020-08-21 11:50:44 +0800

    面试经典

--- src/main/java/com/zxw/leetcode/algorithm/AugustDay.java
+++ src/main/java/com/zxw/leetcode/algorithm/AugustDay.java
@@ -199,9 +199,9 @@ public class AugustDay {
         }
         for (int i = 0; i < s.length(); i++) {
             // 偶数
-            ans += countSubstringsDFS(s,i,i);
+            ans += countSubstringsDFS(s, i, i);
             // 奇数
-            ans += countSubstringsDFS(s,i,i+1);
+            ans += countSubstringsDFS(s, i, i + 1);
         }
         ans += s.length();
         return ans;
@@ -219,5 +219,36 @@ public class AugustDay {
             ans += 1;
         }
         return ans;
+    }
+
+    /**
+     * 111. 二叉树的最小深度
+     * 给定一个二叉树，找出其最小深度。
+     * <p>
+     * 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+     * <p>
+     * 说明: 叶子节点是指没有子节点的节点。
+     * <p>
+     * 示例:
+     * <p>
+     * 给定二叉树 [3,9,20,null,null,15,7],
+     * <p>
+     * 3
+     * / \
+     * 9  20
+     * /  \
+     * 15   7
+     * 返回它的最小深度  2.
+     *
+     * @param root
+     * @return
+     */
+    public int minDepth(TreeNode root) {
+        if(root == null) return 0;
+        int m1 = minDepth(root.left);
+        int m2 = minDepth(root.right);
+        //1.如果左孩子和右孩子有为空的情况，直接返回m1+m2+1
+        //2.如果都不为空，返回较小深度+1
+        return root.left == null || root.right == null ? m1 + m2 + 1 : Math.min(m1,m2) + 1;
     }
 }
