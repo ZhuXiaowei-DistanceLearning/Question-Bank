a277a7dcba4d87f588d3563ef7fa8e79d7673afe
zxw <502513206@qq.com>
2020-10-28 10:21:35 +0800

    面试经典

--- src/main/java/com/zxw/leetcode/algorithm/AugustDay.java
+++ src/main/java/com/zxw/leetcode/algorithm/AugustDay.java
@@ -456,4 +456,25 @@ public class AugustDay {
         Arrays.sort(A);
         return A;
     }
+
+    /**
+     * 1207. 独一无二的出现次数
+     * 给你一个整数数组 arr，请你帮忙统计数组中每个数的出现次数。
+     * 如果每个数的出现次数都是独一无二的，就返回 true；否则返回 false。
+     *  
+     * 示例 1：
+     * 输入：arr = [1,2,2,1,1,3]
+     * 输出：true
+     * 解释：在该数组中，1 出现了 3 次，2 出现了 2 次，3 只出现了 1 次。没有两个数的出现次数相同。
+     *
+     * @param arr
+     * @return
+     */
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+        return map.size() == new HashSet<>(map.values()).size();
+    }
 }
