7e8c25ffaf00c3f988c4b369134c2c2662a29dc4
zxw <502513206@qq.com>
2020-11-05 18:00:18 +0800

    面试经典

---
+++ src/main/java/com/zxw/designpattern/factory/RuleFactory.java
@@ -0,0 +1,14 @@
+package com.zxw.designpattern.factory;
+
+/**
+ * @author zxw
+ * @date 2020/11/5 15:27
+ */
+public class RuleFactory {
+    public RuleService createRuleService(String type){
+        if(type.equals("alarm")){
+            return new AlarmRuleServiceImpl();
+        }
+        return new ForwardRuleServiceImpl();
+    }
+}
---
+++ src/main/java/com/zxw/designpattern/factory/RuleServiceImpl.java
@@ -0,0 +1,12 @@
+package com.zxw.designpattern.factory;
+
+/**
+ * @author zxw
+ * @date 2020/11/5 15:24
+ */
+public class RuleServiceImpl implements RuleService{
+    @Override
+    public void createRule() {
+
+    }
+}
