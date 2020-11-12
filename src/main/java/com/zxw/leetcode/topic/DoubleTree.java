44ded602072748a6b771a90e154fe1a35404d0a9
zxw <502513206@qq.com>
2020-11-12 10:32:03 +0800

    面试经典

--- src/main/java/com/zxw/leetcode/topic/DoubleTree.java
+++ src/main/java/com/zxw/leetcode/topic/DoubleTree.java
@@ -73,4 +73,48 @@ public class DoubleTree {
         connectBFS(n1.right, n2.left);
         connectBFS(n2.left, n2.right);
     }
+
+    /**
+     * 114. 二叉树展开为链表
+     * 给定一个二叉树，原地将它展开为一个单链表。
+     * <p>
+     * <p>
+     * <p>
+     * 例如，给定二叉树
+     * <p>
+     * 1
+     * / \
+     * 2   5
+     * / \   \
+     * 3   4   6
+     *
+     * @param root
+     */
+    public void flatten(TreeNode root) {
+        if (root == null) return;
+
+        flatten(root.left);
+        flatten(root.right);
+
+        /**** 后序遍历位置 ****/
+        // 1、左右子树已经被拉平成一条链表
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+
+        // 2、将左子树作为右子树
+        root.left = null;
+        root.right = left;
+
+        // 3、将原先的右子树接到当前右子树的末端
+        TreeNode p = root;
+        while (p.right != null) {
+            p = p.right;
+        }
+        p.right = right;
+    }
+
+    public void falttenBFS(TreeNode root) {
+
+
+    }
 }
