9c7acfdcdf197834f2a6a8d1bcd70e0352176407
zxw <502513206@qq.com>
2020-12-17 23:46:39 +0800

    面试经典

--- src/main/java/com/zxw/designpattern/decorator/RetryableMongoHttpClient.java
+++ src/main/java/com/zxw/designpattern/decorator/RetryableMongoHttpClient.java
@@ -1,6 +1,7 @@
 package com.zxw.designpattern.decorator;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * @author zxw
@@ -9,6 +10,8 @@ import java.util.List;
 public class RetryableMongoHttpClient extends MongoHttpClientDecorator {
     private final String name;
     private final MongoHttpClientFactory clientFactory;
+    private static final int numberOfRetries = 3;
+    private final AtomicReference<MongoHttpClient> delegate = new AtomicReference<>();
 
     public RetryableMongoHttpClient(String name, MongoHttpClientFactory clientFactory) {
         this.name = name;
@@ -17,10 +20,23 @@ public class RetryableMongoHttpClient extends MongoHttpClientDecorator {
 
     @Override
     protected <R> List<R> execute(RequestExecutor<R> requestExecutor) {
-        return null;
+        for (int i = 0; i < numberOfRetries; i++) {
+            MongoHttpClient mongoHttpClient = delegate.get();
+            if (mongoHttpClient == null) {
+                mongoHttpClient = clientFactory.newClient();
+            }
+            try {
+                List<R> execute = requestExecutor.execute(mongoHttpClient);
+                return execute;
+            } catch (Exception e) {
+
+            }
+            delegate.compareAndSet(mongoHttpClient, null);
+        }
+        throw new RuntimeException("Retry limit reached; giving up on completing the request");
     }
 
-    public static MongoHttpClientFactory createFactory(String name,MongoHttpClientFactory mongoHttpClientFactory) {
+    public static MongoHttpClientFactory createFactory(String name, MongoHttpClientFactory mongoHttpClientFactory) {
         return new MongoHttpClientFactory() {
             @Override
             public MongoHttpClient newClient() {
