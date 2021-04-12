f1202d296ab0da3f60dd572be5a781932ae06459
zxw <502513206@qq.com>
2021-04-12 14:36:21 +0800

    docs: [671]二叉树中第二小的节点

--- src/main/java/com/zxw/leetcode/type/tree/TreeTest.java
+++ src/main/java/com/zxw/leetcode/type/tree/TreeTest.java
@@ -133,8 +133,28 @@ public class TreeTest {
      * @return
      */
     public int findSecondMinimumValue(TreeNode root) {
+        if (root == null || (root.left == null && root.right == null)) {
+            return -1;
+        }
 
-        return 0;
+        int left = root.left.val;
+        int right = root.right.val;
+
+        if (left == root.val) {
+            left = findSecondMinimumValue(root.left);
+        }
+
+        if (right == root.val) {
+            right = findSecondMinimumValue(root.right);
+        }
+
+        if (left != -1 && right != -1) {
+            return Math.min(left, right);
+        }
+        if (left != -1) {
+            return left;
+        }
+        return right;
     }
 
     /**
