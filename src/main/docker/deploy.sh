7e893ef7222b6380c96960966bf75ca8a5fb04e6
zxw <502513206@qq.com>
2021-05-25 17:50:04 +0800

    list.add(root.val);         if(root.left == null && root.right == null && targetSum-root.val == 0){             List<Integer> dest = new ArrayList(Arrays.asList(new Integer[list.size()]));             Collections.copy(dest,list);             res.add(dest);         }

--- src/main/docker/deploy.sh
+++ src/main/docker/deploy.sh
@@ -1,3 +1,3 @@
-#docker build -t apec-java8-images
-#docker tag apec-java8-images harbor.ap-ec.cn/registry/apec-java8-images:1.0.0
-#docker push harbor.ap-ec.cn/registry/apec-java8-images:1.0.0
+#docker build -t xxx
+#docker tag xxx harbor.xx.cn/registry/xx-images:1.0.0
+#docker push harbor.xx.cn/registry/xx-images:1.0.0
