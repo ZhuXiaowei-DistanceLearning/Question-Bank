7fc774d1e60a4c406a024cfc4a3377f4b60d644a
zxw <502513206@qq.com>
2021-08-25 16:01:28 +0800

    javafx

--- unit-test/src/test/java/com/zxw/spock/DataDrivenSpec.groovy
+++ unit-test/src/test/java/com/zxw/spock/DataDrivenSpec.groovy
@@ -1,6 +1,7 @@
 package com.zxw.spock
 
-import spock.lang.Specification;
+import spock.lang.Specification
+import spock.lang.Unroll;
 
 class DataDrivenSpec extends Specification {
     def "maximum of two numbers"() {
@@ -39,5 +40,20 @@ class DataDrivenSpec extends Specification {
         String getSex() {
             name == "Fred" ? "Male" : "Female"
         }
+    }
+
+    @Unroll
+    def "身份证号:#idNo 的生日,性别,年龄是:#result"() {
+        expect: "when + then 组合"
+        IDNumberUtils.getBirAgeSex(idNo) == result
+
+        where: "表格方式测试不同的分支逻辑"
+        idNo                 || result
+        "310168199809187333" || ["birthday": "1998-09-18", "sex": "男", "age": "22"]
+        "320168200212084268" || ["birthday": "2002-12-08", "sex": "女", "age": "18"]
+        "330168199301214267" || ["birthday": "1993-01-21", "sex": "女", "age": "27"]
+        "411281870628201"    || ["birthday": "1987-06-28", "sex": "男", "age": "33"]
+        "427281730307862"    || ["birthday": "1973-03-07", "sex": "女", "age": "47"]
+        "479281691111377"    || ["birthday": "1969-11-11", "sex": "男", "age": "51"]
     }
 }
