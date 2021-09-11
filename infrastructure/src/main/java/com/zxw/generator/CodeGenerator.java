97d5f725a5bc56c95e606f3ad455e1debf9c4321
zxw <502513206@qq.com>
2021-09-11 22:21:21 +0800

    feature: robot

--- infrastructure/src/main/java/com/zxw/generator/CodeGenerator.java
+++ infrastructure/src/main/java/com/zxw/generator/CodeGenerator.java
@@ -17,6 +17,10 @@ import java.util.Scanner;
 // 演示例子，执行 main 方法控制台输入模块表名回车自动生成对应项目目录中
 public class CodeGenerator {
 
+    public static void main(String[] args) {
+        generate("localhost:3306", "ds_0");
+    }
+
     /**
      * <p>
      * 读取控制台内容
@@ -36,34 +40,62 @@ public class CodeGenerator {
         throw new MybatisPlusException("请输入正确的" + tip + "！");
     }
 
-    public static void main(String[] args) {
+    private static void generate(String host, String schema) {
         // 代码生成器
         AutoGenerator mpg = new AutoGenerator();
-
-        // 全局配置
-        GlobalConfig gc = new GlobalConfig();
-        File desktopDir = FileSystemView.getFileSystemView().getHomeDirectory();
-        gc.setOutputDir(desktopDir.getAbsolutePath());
-        gc.setAuthor("zxw");
-        gc.setOpen(false);
+        // 全局设置
+        GlobalConfig gc = getGlobalConfig();
         // gc.setSwagger2(true); 实体属性 Swagger2 注解
-        mpg.setGlobalConfig(gc);
-
-        // 数据源配置
-        DataSourceConfig dsc = new DataSourceConfig();
-        dsc.setUrl("jdbc:mysql://localhost:3306/ds_0?useUnicode=true&useSSL=false&characterEncoding=utf8");
-        // dsc.setSchemaName("public");
-        dsc.setDriverName("com.mysql.jdbc.Driver");
-        dsc.setUsername("root");
-        dsc.setPassword("123456");
-        mpg.setDataSource(dsc);
-
+        // 数据源设置
+        DataSourceConfig dsc = getDataSourceConfig(host, schema);
         // 包配置
-        PackageConfig pc = new PackageConfig();
-//        pc.setModuleName(scanner("模块名"));
-        pc.setParent("mybatis");
+        PackageConfig pc = getPackageConfig();
+        InjectionConfig cfg = getFileOutConfig();
+        TemplateConfig templateConfig = getTemplateConfig();
+        StrategyConfig strategy = getStrategyConfig(pc);
+        mpg.setTemplate(templateConfig);
+        mpg.setGlobalConfig(gc);
+        mpg.setDataSource(dsc);
         mpg.setPackageInfo(pc);
+        mpg.setCfg(cfg);
+        mpg.setStrategy(strategy);
+        mpg.setTemplateEngine(new FreemarkerTemplateEngine());
+        mpg.execute();
+    }
 
+    private static StrategyConfig getStrategyConfig(PackageConfig pc) {
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        strategy.setNaming(NamingStrategy.underline_to_camel);
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+//        strategy.setSuperEntityClass("你自己的父类实体,没有就不用设置!");
+        strategy.setEntityLombokModel(true);
+        strategy.setRestControllerStyle(true);
+        // 公共父类
+//        strategy.setSuperControllerClass("你自己的父类控制器,没有就不用设置!");
+        // 写于父类中的公共字段
+        strategy.setSuperEntityColumns("id");
+        strategy.setInclude(scanner("表名，多个英文逗号分割").split(","));
+        strategy.setControllerMappingHyphenStyle(true);
+        strategy.setTablePrefix(pc.getModuleName() + "_");
+        return strategy;
+    }
+
+    private static TemplateConfig getTemplateConfig() {
+        // 配置模板
+        TemplateConfig templateConfig = new TemplateConfig();
+
+        // 配置自定义输出模板
+        //指定自定义模板路径，注意不要带上.ftl/.vm, 会根据使用的模板引擎自动识别
+        // templateConfig.setEntity("templates/entity2.java");
+        // templateConfig.setService();
+        // templateConfig.setController();
+
+        templateConfig.setXml(null);
+        return templateConfig;
+    }
+
+    private static InjectionConfig getFileOutConfig() {
         // 自定义配置
         InjectionConfig cfg = new InjectionConfig() {
             @Override
@@ -71,11 +103,10 @@ public class CodeGenerator {
                 // to do nothing
             }
         };
-
         // 如果模板引擎是 freemarker
 //        String templatePath = "/templates/mapper.xml.ftl";
         // 如果模板引擎是 velocity
-         String templatePath = "/templates/mapper.xml.vm";
+        String templatePath = "/templates/mapper.xml.vm";
 
         // 自定义输出配置
         List<FileOutConfig> focList = new ArrayList<>();
@@ -104,37 +135,35 @@ public class CodeGenerator {
         });
         */
         cfg.setFileOutConfigList(focList);
-        mpg.setCfg(cfg);
+        return cfg;
+    }
 
-        // 配置模板
-        TemplateConfig templateConfig = new TemplateConfig();
+    private static PackageConfig getPackageConfig() {
+        PackageConfig pc = new PackageConfig();
+//        pc.setModuleName(scanner("模块名"));
+        pc.setParent("mybatis");
+        return pc;
+    }
 
-        // 配置自定义输出模板
-        //指定自定义模板路径，注意不要带上.ftl/.vm, 会根据使用的模板引擎自动识别
-        // templateConfig.setEntity("templates/entity2.java");
-        // templateConfig.setService();
-        // templateConfig.setController();
+    private static DataSourceConfig getDataSourceConfig(String host, String schema) {
+        // 数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setUrl("jdbc:mysql://" + host + "/" + schema + "?useUnicode=true&useSSL=false&characterEncoding=utf8");
+        // dsc.setSchemaName("public");
+        dsc.setDriverName("com.mysql.jdbc.Driver");
+        dsc.setUsername("root");
+        dsc.setPassword("123456");
+        return dsc;
+    }
 
-        templateConfig.setXml(null);
-        mpg.setTemplate(templateConfig);
-
-        // 策略配置
-        StrategyConfig strategy = new StrategyConfig();
-        strategy.setNaming(NamingStrategy.underline_to_camel);
-        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
-        strategy.setSuperEntityClass("你自己的父类实体,没有就不用设置!");
-        strategy.setEntityLombokModel(true);
-        strategy.setRestControllerStyle(true);
-        // 公共父类
-        strategy.setSuperControllerClass("你自己的父类控制器,没有就不用设置!");
-        // 写于父类中的公共字段
-        strategy.setSuperEntityColumns("id");
-        strategy.setInclude(scanner("表名，多个英文逗号分割").split(","));
-        strategy.setControllerMappingHyphenStyle(true);
-        strategy.setTablePrefix(pc.getModuleName() + "_");
-        mpg.setStrategy(strategy);
-        mpg.setTemplateEngine(new FreemarkerTemplateEngine());
-        mpg.execute();
+    private static GlobalConfig getGlobalConfig() {
+        // 全局配置
+        GlobalConfig gc = new GlobalConfig();
+        File desktopDir = FileSystemView.getFileSystemView().getHomeDirectory();
+        gc.setOutputDir(desktopDir.getAbsolutePath());
+        gc.setAuthor("zxw");
+        gc.setOpen(false);
+        return gc;
     }
 
 }
