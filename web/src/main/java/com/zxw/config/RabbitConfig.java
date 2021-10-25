acdbb67cd1a5704b0cded7e9e54ae858117df8f1
zxw <502513206@qq.com>
2021-10-25 14:33:16 +0800

    dev

---
+++ web/src/main/java/com/zxw/config/RabbitConfig.java
@@ -0,0 +1,51 @@
+package com.zxw.config;
+
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.autoconfigure.amqp.RabbitProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author zxw
+ * @date 2021/10/25 13:52
+ */
+@Component
+@EnableRabbit
+public class RabbitConfig {
+    @Bean
+    public ConnectionFactory connectionFactory(RabbitProperties rabbitProperties) {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory(rabbitProperties.getHost());
+        connectionFactory.setUsername(rabbitProperties.getUsername());
+        connectionFactory.setPassword(rabbitProperties.getPassword());
+        connectionFactory.setPort(rabbitProperties.getPort());
+        connectionFactory.setVirtualHost(rabbitProperties.getVirtualHost());
+        return connectionFactory;
+    }
+
+    @Bean
+    public AmqpAdmin amqpAdmin(ConnectionFactory connectionFactory) {
+        return new RabbitAdmin(connectionFactory);
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        return new RabbitTemplate(connectionFactory);
+    }
+
+    //配置消费者监听的容器
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setConcurrentConsumers(3);
+        factory.setMaxConcurrentConsumers(10);
+        //factory.setAcknowledgeMode(AcknowledgeMode.MANUAL);//设置确认模式手工确认
+        return factory;
+    }
+}
