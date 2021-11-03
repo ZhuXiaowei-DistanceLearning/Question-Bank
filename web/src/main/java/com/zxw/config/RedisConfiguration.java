d4077a26a3423703ff3ce6c5dd5636a538e468ad
zxw <502513206@qq.com>
2021-11-03 17:14:33 +0800

    dev

--- web/src/main/java/com/zxw/config/RedisConfiguration.java
+++ web/src/main/java/com/zxw/config/RedisConfiguration.java
@@ -1,5 +1,9 @@
 package com.zxw.config;
 
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -7,6 +11,9 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 import org.springframework.data.redis.core.ReactiveStringRedisTemplate;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author zxw
@@ -34,5 +41,17 @@ public class RedisConfiguration {
         redisTemplate.setHashValueSerializer(stringRedisSerializer);
         redisTemplate.afterPropertiesSet();
         return redisTemplate;
+    }
+
+    @Bean
+    public RedissonClient redissonClient(RedisProperties redisProperties){
+        Config config = new Config();
+        List<String> cluster = redisProperties.getCluster().getNodes()
+                .stream().map(node -> "redis://" + node).collect(Collectors.toList());
+        config.useClusterServers()
+                .setNodeAddresses(cluster);
+//        String redisUrl = String.format("redis://%s:%s",redisProperties.getHost()+"",redisProperties.getPort()+"");
+//        config.useSingleServer().setAddress(redisUrl).setPassword(redisProperties.getPassword());
+        return Redisson.create(config);
     }
 }
