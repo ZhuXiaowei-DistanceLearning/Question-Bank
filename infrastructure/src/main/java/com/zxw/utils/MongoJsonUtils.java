cf592022a4eab371467816a5c918bb7d97d14117
zxw <502513206@qq.com>
2021-12-08 16:43:08 +0800

    dev

---
+++ infrastructure/src/main/java/com/zxw/utils/MongoJsonUtils.java
@@ -0,0 +1,46 @@
+package com.zxw.utils;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.zxw.exception.BusinessException;
+import com.zxw.exception.ExpMsgConsts;
+
+import java.io.*;
+
+/**
+ * @author zxw
+ * @date 2021/12/8 16:30
+ */
+public class MongoJsonUtils {
+    static File file = new File("C:\\Users\\zxw\\Desktop/CONTRACT_SIGN_MASTER_JOB.json");
+
+    public static void main(String[] args) throws Exception {
+        InputStream in;
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            StringBuilder sb = new StringBuilder();
+            String temp;
+            while ((temp = reader.readLine()) != null) {
+                sb.append(temp);
+            }
+            JSONArray jsonObject = JSON.parseArray(sb.toString());
+            System.out.println(jsonObject);
+        }
+    }
+
+    
+
+    public JSONArray getDataArray() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            StringBuilder sb = new StringBuilder();
+            String temp;
+            while ((temp = reader.readLine()) != null) {
+                sb.append(temp);
+            }
+            JSONArray res = JSON.parseArray(sb.toString());
+            return res;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new BusinessException(ExpMsgConsts.COMMON_ERROR, "解析文件失败");
+        }
+    }
+}
