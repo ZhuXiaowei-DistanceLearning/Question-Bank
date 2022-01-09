347191a9e900bb1831903e6c185291b6a04b131d
zxw <502513206@qq.com>
2022-01-09 21:48:48 +0800

    feature: dev

---
+++ netty-reactive/src/main/java/com/zxw/netty/demo/handler/ws/SecureChatServerInitializer.java
@@ -0,0 +1,26 @@
+package com.zxw.netty.demo.handler.ws;
+
+import io.netty.channel.Channel;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslHandler;
+
+import javax.net.ssl.SSLEngine;
+
+public class SecureChatServerInitializer extends ChatServerInitializer {
+    private final SslContext context;
+
+    public SecureChatServerInitializer(ChannelGroup group,
+                                       SslContext context) {
+        super(group);
+        this.context = context;
+    }
+
+    @Override
+    protected void initChannel(Channel ch) throws Exception {
+        super.initChannel(ch);
+        SSLEngine engine = context.newEngine(ch.alloc());
+        engine.setUseClientMode(false);
+        ch.pipeline().addFirst(new SslHandler(engine));
+    }
+}
---
+++ netty-reactive/src/main/java/com/zxw/netty/demo/handler/ws/TextWebSocketFrameHandler.java
@@ -0,0 +1,40 @@
+package com.zxw.netty.demo.handler.ws;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+
+public class TextWebSocketFrameHandler
+        extends SimpleChannelInboundHandler<TextWebSocketFrame> {
+    private final ChannelGroup group;
+
+    public TextWebSocketFrameHandler(ChannelGroup group) {
+        this.group = group;
+    }
+
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx,
+                                   Object evt) throws Exception {
+        if (evt == WebSocketServerProtocolHandler
+                .ServerHandshakeStateEvent.HANDSHAKE_COMPLETE) {
+            //如果该事件表示握手成功，则从该Channelipeline中移除 HttpRequestHandler，因为将不会接收到任何HTTP 消息了
+            ctx.pipeline().remove(HttpRequestHandler.class);
+            //通知所有已经连接的WebSocket 客户端新的客户端已经连接上了
+            group.writeAndFlush(new TextWebSocketFrame(
+                    "Client " + ctx.channel() + " joined"));
+            //将新的 WebSocket Channel添加到 ChannelGroup 中，以便它可以接收到所有的消息
+            group.add(ctx.channel());
+        } else {
+            super.userEventTriggered(ctx, evt);
+        }
+    }
+
+    @Override
+    public void channelRead0(ChannelHandlerContext ctx,
+                             TextWebSocketFrame msg) throws Exception {
+        //增加消息的引用计数，并将它写到 ChannelGroup 中所有已经连接的客户端
+        group.writeAndFlush(msg.retain());
+    }
+}
