ce8e1eb2557aa7789fe0a0f5e7fbe2ccd758043c
zxw <502513206@qq.com>
2022-01-24 22:13:33 +0800

    feature: dev

--- distribute-lock/src/main/java/com/zxw/config/FeignConfig.java
+++ distribute-lock/src/main/java/com/zxw/config/FeignConfig.java
@@ -1,5 +1,6 @@
 package com.zxw.config;
 
+import feign.Request;
 import feign.auth.BasicAuthRequestInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,11 @@ import org.springframework.context.annotation.Configuration;
 public class FeignConfig {
     @Bean
     public BasicAuthRequestInterceptor basicAuthRequestInterceptor() {
-        return new BasicAuthRequestInterceptor("","");
+        return new BasicAuthRequestInterceptor("", "");
+    }
+
+    @Bean
+    public Request.Options options() {
+        return new Request.Options(5000, 1000);
     }
 }
