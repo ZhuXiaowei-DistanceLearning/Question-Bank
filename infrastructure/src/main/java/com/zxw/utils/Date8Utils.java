9060c7a306c97593f60a23048720a7a3ab04ddea
zxw <502513206@qq.com>
2022-03-18 15:36:01 +0800

    feature: dev

--- infrastructure/src/main/java/com/zxw/utils/Date8Utils.java
+++ infrastructure/src/main/java/com/zxw/utils/Date8Utils.java
@@ -15,8 +15,9 @@ public class Date8Utils {
         ZoneId zoneSH = ZoneId.of("Asia/Shanghai");
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         LocalDateTime parse = LocalDateTime.parse("2022-03-09 15:00:00", dateTimeFormatter);
-        LocalDateTime.ofInstant(new Date().toInstant(), ZoneId.systemDefault());
+        LocalDateTime localDateTime = LocalDateTime.ofInstant(new Date().toInstant(), ZoneId.systemDefault());
         System.out.println(parse);
+        System.out.println(localDateTime);
 //        ZonedDateTime of = ZonedDateTime.of(LocalDateTime.parse("", dateTimeFormatter), zoneSH);
 //        ZoneOffset.ofHours(8);
     }
