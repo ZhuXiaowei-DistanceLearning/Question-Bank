426a2353e9ab2b7751c2ba4803fd61313c60dbd6
zxw <502513206@qq.com>
2022-03-19 21:44:58 +0800

    feature: dev

--- infrastructure/src/main/java/com/zxw/utils/MysqlUtils.java
+++ infrastructure/src/main/java/com/zxw/utils/MysqlUtils.java
@@ -2,6 +2,9 @@ package com.zxw.utils;
 
 import com.mysql.jdbc.jdbc2.optional.MysqlXAConnection;
 import com.mysql.jdbc.jdbc2.optional.MysqlXid;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 
 import javax.transaction.xa.XAException;
 import javax.transaction.xa.XAResource;
@@ -16,38 +19,74 @@ import java.util.UUID;
  * @author zxw
  * @date 2022/3/19 17:46
  */
+@Slf4j
 public class MysqlUtils {
 
 
     public static void main(String[] args) throws SQLException {
-        Connection connect = getConnect("jdbc:mysql://127.0.0.1:3306/fescar?characterEncoding=utf-8");
-        Connection connect2 = getConnect("jdbc:mysql://127.0.0.1:3306/fescar?characterEncoding=utf-8");
-        MysqlXAConnection xaConnect = getXAConnect(connect);
-        XAResource rm1 = xaConnect.getXAResource();
-        MysqlXAConnection xaConnect2 = getXAConnect(connect);
-        XAResource rm2 = xaConnect2.getXAResource();
+        Connection connect = getConnect("jdbc:mysql://127.0.0.1:3306/fescar?characterEncoding=utf-8&useSSL=false");
+        Connection connect2 = getConnect("jdbc:mysql://127.0.0.1:3306/fescar_2?characterEncoding=utf-8&useSSL=false");
+        XAResource rm1 = getXAResource(getXAConnect(connect));
+        XAResource rm2 = getXAResource(getXAConnect(connect2));
         byte[] gtrId = UUID.randomUUID().toString().getBytes();
-        int xaRes1 = executeXA(gtrId, rm1, "INSERT INTO `fescar`.`account_tbl` (`id`, `user_id`, `money`) VALUES (1, 'U100000', 1000);\n", connect);
-        int xaRes2 = executeXA(gtrId, rm2, "", connect2);
-
+        XAResult xaRes1 = executeXA(gtrId, rm1, "update storage_tbl set count = 110 where id = 30", connect);
+        XAResult xaRes2 = executeXA(gtrId, rm2, "update storage_tbl set count = 110 where id = 30", connect2);
+        if (xaRes1.getStatus() == XAResource.XA_OK && xaRes1.getStatus() == XAResource.XA_OK) {
+            commit(rm1, xaRes1.getXid());
+            commit(rm2, xaRes2.getXid());
+        } else {
+            rollback(rm1, xaRes1.getXid());
+            rollback(rm2, xaRes2.getXid());
+        }
+        connect.close();
+        connect2.close();
     }
 
-    public static int executeXA(byte[] gtrId, XAResource rm, String sql, Connection connect) {
+    public static void rollback(XAResource xaResource, Xid xid) {
+        try {
+            xaResource.rollback(xid);
+        } catch (XAException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void commit(XAResource xaResource, Xid xid) {
+        try {
+            xaResource.commit(xid, true);
+        } catch (XAException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static XAResult executeXA(byte[] gtrId, XAResource rm, String sql, Connection connect) {
         int status = -1;
         byte[] b1Id = UUID.randomUUID().toString().getBytes();
         Xid xid1 = new MysqlXid(gtrId, b1Id, 1);
+        PreparedStatement preparedStatement = null;
         try {
             rm.start(xid1, XAResource.TMNOFLAGS);
-            PreparedStatement preparedStatement = connect.prepareStatement(sql);
-            preparedStatement.execute();
-            rm.end(xid1, XAResource.TMSUCCESS);
+            preparedStatement = connect.prepareStatement(sql);
+            try {
+                log.info("开始执行sql");
+                preparedStatement.executeUpdate(sql);
+                rm.end(xid1, XAResource.TMSUCCESS);
+            } catch (SQLException e) {
+                e.printStackTrace();
+                rm.end(xid1, XAResource.TMFAIL);
+            }
             status = rm.prepare(xid1);
         } catch (XAException e) {
             e.printStackTrace();
         } catch (SQLException throwables) {
             throwables.printStackTrace();
+        } finally {
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
-        return status;
+        return new XAResult(status, xid1);
     }
 
     public static Connection getConnect(String url) {
@@ -69,5 +108,22 @@ public class MysqlUtils {
             e.printStackTrace();
         }
         return xaConnection;
+    }
+
+    public static XAResource getXAResource(MysqlXAConnection connection) {
+        XAResource xaResource = null;
+        try {
+            xaResource = connection.getXAResource();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return xaResource;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class XAResult {
+        int status;
+        Xid xid;
     }
 }
