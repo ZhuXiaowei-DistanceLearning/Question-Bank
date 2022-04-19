0940ba9853fd07d0df6c5f1b6b503afc7876a56c
zxw <502513206@qq.com>
2022-04-19 11:17:43 +0800

    feature: dev

---
+++ web/src/main/java/com/zxw/service/ServerLogService.java
@@ -0,0 +1,8 @@
+package com.zxw.service;
+
+/**
+ * @author zxw
+ * @date 2022/4/15 9:34
+ */
+public interface ServerLogService {
+}
---
+++ web/src/main/java/com/zxw/service/impl/ServerLogServiceImpl.java
@@ -0,0 +1,12 @@
+package com.zxw.service.impl;
+
+import com.zxw.service.ServerLogService;
+import lombok.Data;
+
+/**
+ * @author zxw
+ * @date 2022/4/15 9:34
+ */
+@Data
+public class ServerLogServiceImpl implements ServerLogService {
+}
