178f9ebac58f325f9db516d18a09af9396d9ae53
zxw <502513206@qq.com>
2022-06-02 16:56:38 +0800

    feature: dev

---
+++ distribute-lock/src/main/java/com/zxw/condition/GuardedObject.java
@@ -0,0 +1,46 @@
+package com.zxw.condition;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.function.Predicate;
+
+class GuardedObject<T> {
+    //受保护的对象
+    T obj;
+    final Lock lock =
+            new ReentrantLock();
+    final Condition done =
+            lock.newCondition();
+    final int timeout = 1;
+
+    //获取受保护对象
+    T get(Predicate<T> p) {
+        lock.lock();
+        try {
+            //MESA管程推荐写法
+            while (!p.test(obj)) {
+                done.await(timeout,
+                        TimeUnit.SECONDS);
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            lock.unlock();
+        }
+        //返回非空的受保护对象
+        return obj;
+    }
+
+    //事件通知方法
+    void onChanged(T obj) {
+        lock.lock();
+        try {
+            this.obj = obj;
+            done.signalAll();
+        } finally {
+            lock.unlock();
+        }
+    }
+}
