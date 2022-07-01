7a8751fd28b465445fe7a261f1890d4ef65e09fb
zxw <502513206@qq.com>
2022-07-01 10:04:14 +0800

    feature: dev

--- algorithm/src/main/java/com/zxw/leetcode/topic/SearchAlgor.java
+++ algorithm/src/main/java/com/zxw/leetcode/topic/SearchAlgor.java
@@ -1,6 +1,10 @@
 package com.zxw.leetcode.topic;
 
 import cn.hutool.core.lang.Assert;
+import cn.hutool.core.util.ArrayUtil;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author zxw
@@ -10,7 +14,60 @@ public class SearchAlgor {
 
     public static void main(String[] args) {
         SearchAlgor searchAlgor = new SearchAlgor();
-        Assert.isTrue(0 == searchAlgor.search(new int[]{4,5,6,7,0,1,2}, 0));
+        List<Integer> list = new ArrayList<>();
+        list.stream().mapToInt(Integer::valueOf).toArray();
+//        Assert.isTrue(0 == searchAlgor.search(new int[]{4,5,6,7,0,1,2}, 0));
+        Assert.isTrue(ArrayUtil.equals(new int[]{1, 2}, searchAlgor.searchRange(new int[]{1, 2, 2}, 2)));
+        Assert.isTrue(ArrayUtil.equals(new int[]{3, 4}, searchAlgor.searchRange(new int[]{5, 7, 7, 8, 8, 10}, 8)));
+        Assert.isTrue(ArrayUtil.equals(new int[]{-1, -1}, searchAlgor.searchRange(new int[]{5, 7, 7, 8, 8, 10}, 6)));
+        Assert.isTrue(ArrayUtil.equals(new int[]{0, 1}, searchAlgor.searchRange(new int[]{2, 2}, 2)));
+    }
+
+    public int[] searchRange(int[] nums, int target) {
+        return new int[]{left_bound(nums, target), right_bound(nums, target)};
+    }
+
+    int left_bound(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        // 搜索区间为 [left, right]
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                // 搜索区间变为 [mid+1, right]
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                // 搜索区间变为 [left, mid-1]
+                right = mid - 1;
+            } else if (nums[mid] == target) {
+                // 收缩右侧边界
+                right = mid - 1;
+            }
+        }
+        // 检查出界情况
+        if (left >= nums.length || nums[left] != target) {
+            return -1;
+        }
+        return left;
+    }
+
+    int right_bound(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] == target) {
+                // 这里改成收缩左侧边界即可
+                left = mid + 1;
+            }
+        }
+        // 这里改为检查 right 越界的情况，见下图
+        if (right < 0 || nums[right] != target) {
+            return -1;
+        }
+        return right;
     }
 
     public int search(int[] nums, int target) {
@@ -32,7 +89,7 @@ public class SearchAlgor {
                 // 代表该值在左区间中
                 if (target >= nums[0] && target < nums[mid]) {
                     hi = mid - 1;
-                }else{
+                } else {
                     lo = mid + 1;
                 }
             } else {
