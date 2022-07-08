f0e2ed93d5a00f922b271be30516460899425f0c
zxw <502513206@qq.com>
2022-07-08 11:22:59 +0800

    feature: dev

--- algorithm/src/main/java/com/zxw/leetcode/topic/TwoArray.java
+++ algorithm/src/main/java/com/zxw/leetcode/topic/TwoArray.java
@@ -1,5 +1,8 @@
 package com.zxw.leetcode.topic;
 
+import cn.hutool.core.lang.Assert;
+import com.zxw.leetcode.type.tree.LeetCodeWrapper;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -13,28 +16,82 @@ public class TwoArray {
 
     public static void main(String[] args) {
         TwoArray twoArray = new TwoArray();
-        twoArray.groupAnagrams(new String[]{"eat","tea","tan","ate","nat","bat"});
+        Assert.isTrue(twoArray.minPathSum(LeetCodeWrapper.stringToInt2dArray("[[1,3,1],[1,5,1],[4,2,1]]")) == 7);
+        twoArray.groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"});
+    }
+
+    /**
+     * 64.最小路径和
+     *
+     * @param grid
+     * @return
+     */
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = grid[i][0] + dp[i - 1][0];
+        }
+        for (int i = 1; i < n; i++) {
+            dp[0][i] = grid[0][i] + dp[0][i - 1];
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = grid[i][j] + Math.min(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
+    /**
+     * 62.不同路径
+     *
+     * @param m
+     * @param n
+     * @return
+     */
+    public int uniquePaths(int m, int n) {
+        if (m <= 1 || n <= 1) {
+            return 1;
+        }
+        int[][] dp = new int[m][n];
+        dp[0][1] = 1;
+        dp[1][0] = 1;
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 0; i < n; i++) {
+            dp[0][i] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
     }
 
     public List<List<String>> groupAnagrams(String[] strs) {
         List<List<String>> res = new ArrayList<>();
-        if(strs.length<=1){
+        if (strs.length <= 1) {
             res.add(List.of(strs[0]));
             return res;
         }
         Map<String, List<String>> map = new HashMap<>();
         for (int i = 0; i < strs.length; i++) {
             String s = encode(strs[i]);
-            List<String> list = map.computeIfAbsent(s,e -> new ArrayList<>());
+            List<String> list = map.computeIfAbsent(s, e -> new ArrayList<>());
             list.add(strs[i]);
         }
-        map.forEach((k,v)->{
+        map.forEach((k, v) -> {
             res.add(v);
         });
         return res;
     }
 
-    public String encode(String s){
+    public String encode(String s) {
         char[] c = new char[26];
         for (int i = 0; i < s.length(); i++) {
             c[s.charAt(i) - 'a']++;
@@ -44,10 +101,11 @@ public class TwoArray {
 
     /**
      * 48.旋转图像
+     *
      * @param matrix
      */
     public void rotate(int[][] matrix) {
-        if(matrix.length == 0){
+        if (matrix.length == 0) {
             return;
         }
         int n = matrix.length;
--- algorithm/src/main/java/com/zxw/leetcode/type/tree/LeetCodeWrapper.java
+++ algorithm/src/main/java/com/zxw/leetcode/type/tree/LeetCodeWrapper.java
@@ -1,8 +1,10 @@
 package com.zxw.leetcode.type.tree;
 
+import com.eclipsesource.json.JsonArray;
 import com.zxw.datastruct.ListNode;
 import com.zxw.datastruct.TreeNode;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -11,6 +13,39 @@ public class LeetCodeWrapper {
     public static void main(String[] args) {
         TreeNode root = LeetCodeWrapper.stringToTreeNode("[1,3,2,5,3,null,9]");
         LeetCodeWrapper.prettyPrintTree(root);
+    }
+
+    public static String int2dArrayToString(int[][] array) {
+        if (array == null) {
+            return "null";
+        }
+        if (array.length == 0) {
+            return "[]";
+        }
+
+        StringBuilder sb = new StringBuilder("[");
+        for (int[] item : array) {
+            sb.append(Arrays.toString(item));
+            sb.append("\n");
+        }
+
+        sb.setCharAt(sb.length() - 1, ']');
+        return sb.toString();
+    }
+
+    public static int[][] stringToInt2dArray(String input) {
+        JsonArray jsonArray = JsonArray.readFrom(input);
+        if (jsonArray.size() == 0) {
+            return new int[0][0];
+        }
+
+        int[][] arr = new int[jsonArray.size()][];
+        for (int i = 0; i < arr.length; i++) {
+            JsonArray cols = jsonArray.get(i).asArray();
+            arr[i] = stringToIntegerArray(cols.toString());
+        }
+        System.out.println(int2dArrayToString(arr));
+        return arr;
     }
 
     public static int[] stringToIntegerArray(String input) {
@@ -22,7 +57,7 @@ public class LeetCodeWrapper {
 
         String[] parts = input.split(",");
         int[] output = new int[parts.length];
-        for(int index = 0; index < parts.length; index++) {
+        for (int index = 0; index < parts.length; index++) {
             String part = parts[index].trim();
             output[index] = Integer.parseInt(part);
         }
@@ -36,7 +71,7 @@ public class LeetCodeWrapper {
         // Now convert that list into linked list
         ListNode dummyRoot = new ListNode(0);
         ListNode ptr = dummyRoot;
-        for(int item : nodeValues) {
+        for (int item : nodeValues) {
             ptr.next = new ListNode(item);
             ptr = ptr.next;
         }
@@ -46,36 +81,36 @@ public class LeetCodeWrapper {
     /**
      * Definition for a binary tree node.
      * public class TreeNode {
-     *     int val;
-     *     TreeNode left;
-     *     TreeNode right;
-     *     TreeNode(int x) { val = x; }
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode(int x) { val = x; }
      * }
      */
-    
+
     public static String treeNodeToString(TreeNode root) {
         if (root == null) {
             return "[]";
         }
-    
+
         String output = "";
         Queue<TreeNode> nodeQueue = new LinkedList<>();
         nodeQueue.add(root);
-        while(!nodeQueue.isEmpty()) {
+        while (!nodeQueue.isEmpty()) {
             TreeNode node = nodeQueue.remove();
-    
+
             if (node == null) {
-              output += "null, ";
-              continue;
+                output += "null, ";
+                continue;
             }
-    
+
             output += String.valueOf(node.val) + ", ";
             nodeQueue.add(node.left);
             nodeQueue.add(node.right);
         }
         return "[" + output.substring(0, output.length() - 2) + "]";
     }
-    
+
     public static TreeNode stringToTreeNode(String input) {
 
         input = input.trim();
@@ -83,21 +118,21 @@ public class LeetCodeWrapper {
         if (input.length() == 0) {
             return null;
         }
-    
+
         String[] parts = input.split(",");
         String item = parts[0];
         TreeNode root = new TreeNode(Integer.parseInt(item));
         Queue<TreeNode> nodeQueue = new LinkedList<>();
         nodeQueue.add(root);
-    
+
         int index = 1;
-        while(!nodeQueue.isEmpty()) {
+        while (!nodeQueue.isEmpty()) {
             TreeNode node = nodeQueue.remove();
-    
+
             if (index == parts.length) {
                 break;
             }
-    
+
             item = parts[index++];
             item = item.trim();
             if (!item.equals("null")) {
@@ -105,11 +140,11 @@ public class LeetCodeWrapper {
                 node.left = new TreeNode(leftNumber);
                 nodeQueue.add(node.left);
             }
-    
+
             if (index == parts.length) {
                 break;
             }
-    
+
             item = parts[index++];
             item = item.trim();
             if (!item.equals("null")) {
@@ -120,26 +155,26 @@ public class LeetCodeWrapper {
         }
         return root;
     }
-    
+
     public static void prettyPrintTree(TreeNode node, String prefix, boolean isLeft) {
         if (node == null) {
             System.out.println("Empty tree");
             return;
         }
-    
+
         if (node.right != null) {
             prettyPrintTree(node.right, prefix + (isLeft ? "│   " : "    "), false);
         }
-    
+
         System.out.println(prefix + (isLeft ? "└── " : "┌── ") + node.val);
-    
+
         if (node.left != null) {
             prettyPrintTree(node.left, prefix + (isLeft ? "    " : "│   "), true);
         }
     }
-    
+
     public static void prettyPrintTree(TreeNode node) {
-        prettyPrintTree(node,  "", true);
+        prettyPrintTree(node, "", true);
     }
-    
+
 }
