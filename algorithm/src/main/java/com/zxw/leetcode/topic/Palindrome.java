7d79d59387acb88f026cbdcf8b99211b4f5f5580
zxw <502513206@qq.com>
2022-08-06 00:05:34 +0800

    feature: dev

--- algorithm/src/main/java/com/zxw/leetcode/topic/Palindrome.java
+++ algorithm/src/main/java/com/zxw/leetcode/topic/Palindrome.java
@@ -1,6 +1,7 @@
 package com.zxw.leetcode.topic;
 
 import com.zxw.datastruct.ListNode;
+import com.zxw.leetcode.type.tree.LeetCodeWrapper;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -10,15 +11,29 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Palindrome {
     public static void main(String[] args) {
-
+        Palindrome palindrome = new Palindrome();
+        palindrome.isPalindrome(LeetCodeWrapper.stringToListNode("[1,2,2,1]"));
     }
 
     /**
      * 234.回文链表
+     *
      * @param head
      * @return
      */
+    ListNode slow = null;
+
     public boolean isPalindrome(ListNode head) {
-        return true;
+        slow = head;
+        return traverse(head);
+    }
+
+    public boolean traverse(ListNode head) {
+        if (head == null) {
+            return true;
+        }
+        boolean res = traverse(head.next) && (head.val == slow.val);
+        slow = slow.next;
+        return res;
     }
 }
