60442328b65c6783c65dda46ed5a8d5d9df0dff1
zxw <502513206@qq.com>
2022-08-05 22:53:38 +0800

    feature: dev

---
+++ algorithm/src/main/java/com/zxw/leetcode/topic/Palindrome.java
@@ -0,0 +1,24 @@
+package com.zxw.leetcode.topic;
+
+import com.zxw.datastruct.ListNode;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author zxw
+ * @date 2022/8/5 16:55
+ */
+@Slf4j
+public class Palindrome {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 234.回文链表
+     * @param head
+     * @return
+     */
+    public boolean isPalindrome(ListNode head) {
+        return true;
+    }
+}
