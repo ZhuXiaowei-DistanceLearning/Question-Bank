3ccdeb98e8ce02d63062163e36abb17295dcc469
zxw <502513206@qq.com>
2022-08-30 13:50:44 +0800

    feature: dev

---
+++ algorithm/src/main/java/com/zxw/leetcode/topic/Heap.java
@@ -0,0 +1,41 @@
+package com.zxw.leetcode.topic;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+/**
+ * @author zxw
+ * @date 2022/8/30 11:28
+ */
+public class Heap {
+    public static void main(String[] args) {
+        Heap heap = new Heap();
+    }
+
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> occurrences = new HashMap<Integer, Integer>();
+        for (int num : nums) {
+            occurrences.put(num, occurrences.getOrDefault(num, 0) + 1);
+        }
+
+        // int[] 的第一个元素代表数组的值，第二个元素代表了该值出现的次数
+        PriorityQueue<int[]> queue = new PriorityQueue<>((m, n) -> m[1] - n[1]);
+        for (Map.Entry<Integer, Integer> entry : occurrences.entrySet()) {
+            int num = entry.getKey(), count = entry.getValue();
+            if (queue.size() == k) {
+                if (queue.peek()[1] < count) {
+                    queue.poll();
+                    queue.offer(new int[]{num, count});
+                }
+            } else {
+                queue.offer(new int[]{num, count});
+            }
+        }
+        int[] ret = new int[k];
+        for (int i = 0; i < k; ++i) {
+            ret[i] = queue.poll()[0];
+        }
+        return ret;
+    }
+}
