461af9d57d4d74e4841e282ca5df1ef8bc2faf56
zxw <502513206@qq.com>
2022-09-11 13:07:52 +0800

    feature: dev

--- algorithm/src/main/java/com/zxw/leetcode/topic/BackTrace.java
+++ algorithm/src/main/java/com/zxw/leetcode/topic/BackTrace.java
@@ -1,5 +1,6 @@
 package com.zxw.leetcode.topic;
 
+import cn.hutool.core.lang.Assert;
 import com.zxw.leetcode.type.tree.LeetCodeWrapper;
 
 import java.util.*;
@@ -23,7 +24,8 @@ public class BackTrace {
 
     public static void main(String[] args) {
         BackTrace backTrace = new BackTrace();
-        backTrace.wordBreak("catsandog", LeetCodeWrapper.stringToStringArrayList("[cats,dog,sand,and,cat]"));
+        Assert.isTrue(2 == backTrace.subarraySum(LeetCodeWrapper.stringToIntegerArray("[1,1,1]"), 2));
+//        backTrace.wordBreak("catsandog", LeetCodeWrapper.stringToStringArrayList("[cats,dog,sand,and,cat]"));
 //        backTrace.wordBreak("leetcode", LeetCodeWrapper.stringToStringArrayList("[leet,code]"));
 //        backTrace.permute(new int[]{1, 2, 3});
     }
@@ -287,10 +289,10 @@ public class BackTrace {
     }
 
     public int findTargetSumWaysBacktrack(int[] nums, int target, int start, int sum) {
-        if(start == nums.length){
-            if(sum == target){
+        if (start == nums.length) {
+            if (sum == target) {
                 return 1;
-            }else {
+            } else {
                 return 0;
             }
         }
@@ -319,6 +321,57 @@ public class BackTrace {
         return result;
     }
 
+    private Integer resInt = 0;
+
+    /**
+     * 560.和为K的子数组
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int subarraySum(int[] nums, int k) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        HashMap<Integer,Integer> map = new HashMap<>();
+        //细节，这里需要预存前缀和为 0 的情况，会漏掉前几位就满足的情况
+        //例如输入[1,1,0]，k = 2 如果没有这行代码，则会返回0,漏掉了1+1=2，和1+1+0=2的情况
+        //输入：[3,1,1,0] k = 2时则不会漏掉
+        //因为presum[3] - presum[0]表示前面 3 位的和，所以需要map.put(0,1),垫下底
+        map.put(0, 1);
+        int count = 0;
+        int presum = 0;
+        for (int x : nums) {
+            presum += x;
+            //当前前缀和已知，判断是否含有 presum - k的前缀和，那么我们就知道某一区间的和为 k 了。
+            if (map.containsKey(presum - k)) {
+                count += map.get(presum - k);//获取次数
+            }
+            //更新
+            map.put(presum,map.getOrDefault(presum,0) + 1);
+        }
+        return count;
+    }
+
+    public int subarraySumBackTrack(int[] nums, int k, int start) {
+        // 把它俩转成字符串才能作为哈希表的键
+        if (nums.length == start) {
+            return 0;
+        }
+        int k1 = k - nums[start];
+        String key = k1 + "";
+        if (memo.containsKey(key)) {
+            return memo.get(key);
+        }
+        if (k1 == 0) {
+            memo.put(key, 1);
+            resInt += 1;
+        }
+        int i = subarraySumBackTrack(nums, k1, start + 1);
+        memo.put(key, memo.getOrDefault(key, 0));
+        return i;
+    }
 
 
 }
