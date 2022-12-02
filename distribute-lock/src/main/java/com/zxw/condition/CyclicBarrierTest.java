ce475b6a65fce24bf699f11514f943d4eb4eee9e
zxw <502513206@qq.com>
2022-12-02 16:14:30 +0800

    feature: dev

--- distribute-lock/src/main/java/com/zxw/condition/CyclicBarrierTest.java
+++ distribute-lock/src/main/java/com/zxw/condition/CyclicBarrierTest.java
@@ -2,46 +2,61 @@ package com.zxw.condition;
 
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * CyclicBarrier是一组线程之间互相等待
+ *
  * @author zxw
  * @date 2022/5/31 17:37
  */
 public class CyclicBarrierTest {
     public static void main(String[] args) {
+        ExecutorService executorService = Executors.newCachedThreadPool();
         CyclicBarrier countDownLatch = new CyclicBarrier(2, () -> {
-            System.out.println("执行完毕");
+            executorService.execute(()->{
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("执行完毕");
+            });
         });
         new Thread(() -> {
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+            while (true) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                try {
+                    countDownLatch.await();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                } catch (BrokenBarrierException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("t1");
             }
-            try {
-                countDownLatch.await();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            } catch (BrokenBarrierException e) {
-                throw new RuntimeException(e);
-            }
-            System.out.println("t1");
         }, "t1").start();
         new Thread(() -> {
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+            while (true) {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                try {
+                    countDownLatch.await();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                } catch (BrokenBarrierException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("t2");
             }
-            try {
-                countDownLatch.await();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            } catch (BrokenBarrierException e) {
-                throw new RuntimeException(e);
-            }
-            System.out.println("t2");
         }, "t2").start();
         System.out.println("等待");
     }
